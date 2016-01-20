3533a47eb65c4c23c06be897ee60a190a2d0e14b
Radarn <admin@Arthurs-dator.lan>
2016-01-20 14:14:53 +0100

    made my first AJAX request.. The coordinates for sweetness location is not 100%

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-
+$(".warm").click(function(){
+	var swedenWeather = "http://api.openweathermap.org/data/2.5/find?lat=62.7&lon=16.2&cnt=20&APPID=c228f71bf091522167799172d83450d3";
+	$.getJSON(swedenWeather, function(data){
+		console.log(data.message);
+	}); //End getJSON
+}); //End click
 
 
 //API KEY : "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=c228f71bf091522167799172d83450d3"
+//putting options in URL : "http://api.openweathermap.org/data/2.5/find?lat=62.7&lon=16.2&cnt=20&APPID=c228f71bf091522167799172d83450d3"
