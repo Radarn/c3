930ef9c682fcce62756643251e31f414f2c281ec
Radarn <admin@Arthurs-dator.lan>
2016-01-20 15:08:49 +0100

    Think I found a way to show the highest and lowest temperature
    Using Math.max() and Math.min() on the array containing all the
    temperatures. For some reason they both return the same value, the
    highest, trying to figure it out..

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 $(".warm").click(function(){
+	var arr = [];
 	var swedenWeather = "http://api.openweathermap.org/data/2.5/find?lat=62.7&lon=16.2&cnt=20&APPID=c228f71bf091522167799172d83450d3";
 	$.getJSON(swedenWeather, function(data){
-		console.log(data.message);
+		$(data.list).each(function(index, element){
+			
+			arr.push(element.main.temp);
+			/*$(element1.main.temp).each(function(index, element){
+				//console.log(element);				
+				if (se > this){
+					alert("hej");
+				} else if (se < this) {
+					alert("okay");
+				}
+			}) // End nested each*/
+		}); //End each loop
+		console.log(arr);
+		console.log(Math.max(parseInt(arr)));
+		console.log(Math.min(parseInt(arr)))
+
 	}); //End getJSON
 }); //End click
 
+//Finding highest and lowest degree logic:
+//val() higher then all the other val() = highest temperature
+//val() lower then all the other val() = lowest temperature
 
 //API KEY : "http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=c228f71bf091522167799172d83450d3"
 //putting options in URL : "http://api.openweathermap.org/data/2.5/find?lat=62.7&lon=16.2&cnt=20&APPID=c228f71bf091522167799172d83450d3"
