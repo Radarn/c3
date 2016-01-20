4ceaf20db40c17ab75d830d25466ce941c0ca473
Radarn <admin@Arthurs-dator.lan>
2016-01-20 15:27:37 +0100

    Added Array.min and Array.max functions to get the highest and lowest temperature

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
+var arr = [];
 $(".warm").click(function(){
-	var arr = [];
 	var swedenWeather = "http://api.openweathermap.org/data/2.5/find?lat=62.7&lon=16.2&cnt=20&APPID=c228f71bf091522167799172d83450d3";
 	$.getJSON(swedenWeather, function(data){
 		$(data.list).each(function(index, element){
-			
 			arr.push(element.main.temp);
-			/*$(element1.main.temp).each(function(index, element){
-				//console.log(element);				
-				if (se > this){
-					alert("hej");
-				} else if (se < this) {
-					alert("okay");
-				}
-			}) // End nested each*/
 		}); //End each loop
-		console.log(arr);
-		console.log(Math.max(parseInt(arr)));
-		console.log(Math.min(parseInt(arr)))
-
+		Array.min(arr);
+		Array.max(arr);
 	}); //End getJSON
 }); //End click
+
+Array.min = function( array ){
+	console.log(Math.min.apply(Math, array))
+    return Math.min.apply(Math, array);
+};
+
+Array.max = function( array ){
+	console.log(Math.max.apply(Math, array))
+    return Math.max.apply(Math, array);
+};
+
 
 //Finding highest and lowest degree logic:
 //val() higher then all the other val() = highest temperature
