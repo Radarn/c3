4ab32ad0dc9936679ee0c10dae518ba1b8607280
Radarn <admin@Arthurs-dator.lan>
2016-01-24 15:40:45 +0100

    added geolocation so it finds users position

--- js/script.js
+++ js/script.js
@@ -1,9 +1,26 @@
 var arr = [];
 var map;
+var coords = {};
+window.onload = function(){
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(function(position){
+			console.log(position);
+			coords.lat = position.coords.latitude
+			coords.lon = position.coords.longitude
+			$(".btn-section").show();
+		}); //End anonymous function
+	} else {
+		alert("Your browser does not support geolocation");
+	};
+}
+
+
 $("button").click(function(){
 	var id = this.id;
-	var swedenWeather = "http://api.openweathermap.org/data/2.5/find?lat=62.7&lon=16.2&cnt=20&APPID=c228f71bf091522167799172d83450d3";
-	$.getJSON(swedenWeather, function(data){
+	var lat = coords.lat;
+	var lon = coords.lon;
+	var weather = "http://api.openweathermap.org/data/2.5/find?lat=" + lat + "&lon=" + lon + "&cnt=50&APPID=c228f71bf091522167799172d83450d3";
+	$.getJSON(weather, function(data){
 		$(data.list).each(function(index, element){
 
 			//We want the lon and lat, the name and the temp data
@@ -63,6 +80,7 @@ $("button").click(function(){
 }); //End click
 
 var initMap = function(obj){
+
 	map = new google.maps.Map(document.getElementById("map"), {
 		center: {lat: obj.lat, lng: obj.lon},
 		zoom: 10
