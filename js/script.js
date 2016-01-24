9eb89a2828948a8b4a3beb421a4da41745941569
Radarn <admin@Arthurs-dator.lan>
2016-01-24 14:11:43 +0100

    added GoogleMaps API and made so it shows the warmest city when button is clicked!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var arr = [];
-var arr2 = [];
+var map;
+var string = "";
 $(".warm").click(function(){
 	var swedenWeather = "http://api.openweathermap.org/data/2.5/find?lat=62.7&lon=16.2&cnt=20&APPID=c228f71bf091522167799172d83450d3";
 	$.getJSON(swedenWeather, function(data){
@@ -16,24 +17,48 @@ $(".warm").click(function(){
 		//Checking the lowest and highest temp in the array.
 
 		var lowestTemp = Math.min.apply(Math, arr);
-		var highestTemp = Math.max.apply(Math, arr);
+		var highestTemp = Math.max.apply(Math, arr);	
 
 		//Loops through the data.list elements again and check if any of their
 		//temperatues matches the lowest/highest temp variables. If so
 		//create an object containing the information we need about that element
 
+		var pickFirstCity = 0;
 		$(data.list).each(function(index, element){
 			if (element.main.temp === highestTemp) {
-				var obj = {
-					name: element.name,
-					lon: element.coord.lon,
-					lat: element.coord.lat
+				//Makes the function pick the first one in the list if there are
+				//multiply cities with the same (warmest) degree.
+				if (pickFirstCity === 0) {
+					var obj = {
+						name: element.name,
+						lon: element.coord.lon,
+						lat: element.coord.lat,
+						temp: element.main.temp
+					};
+					pickFirstCity++;
 				};
-				return console.log(obj);
+				$("#map, .back").show();
+				return initMap(obj);
 			};
 		}); // End each loop
 	}); //End getJSON
 }); //End click
+
+var initMap = function(obj){
+	map = new google.maps.Map(document.getElementById("map"), {
+		center: {lat: obj.lat, lng: obj.lon},
+		zoom: 10
+	});
+	$(".info-text").text("");
+	$(".info-text").text("The warmest city in Sweden right now is " + obj.name + " with " + obj.temp + " degrees!");
+	$(".cold, .warm").hide();
+	$(".back").show();
+	$(".back").click(function(){
+		$(".cold, .warm").show();
+		$("#map, .back").hide();
+	});
+	
+};
 
 Array.min = function(array){
 	console.log(Math.min.apply(Math, arr));
